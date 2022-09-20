1f3b369ac97201a52d03298ee9c41f628362acc3
Z-TankerWOTB <79827843+Z-TankerWOTB@users.noreply.github.com>
2022-09-20 14:51:20 +0530

    Add files via upload

--- Image_recognizer/main.js
+++ Image_recognizer/main.js
@@ -22,4 +22,21 @@ function identify(){
 function modelLoaded(){
     console.log("Model Loaded");
 }
+
+function identify(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error , results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('result_object_name').innerHTML = results[0].label ;
+        document.getElementById('result_object_accuracy').innerHTML = results[0].confidence.toFixed(2);
+    }
+}
+
 // ' https://teachablemachine.withgoogle.com/models/89sIFzPtO/ '
